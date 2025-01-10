b9a7e5ef15da3351338352a365cd3a59842b1a51
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2025-01-10 13:16:31 -0800

    1-100

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 words = []
 
-for i in range(100000):
-    pass
+def number_to_name(n):
+    zero=["zero"]
+    ones=["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    teens=["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    tens=["","","twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "]
+    
+    return tens[n // 10] + (teens[n%10] if n%100//10 == 1 else ones[n%10]) + ("zero" if n == 0 else "")
+
+for i in range(90):
+    print(i, ": ", number_to_name(i))
+
+
+

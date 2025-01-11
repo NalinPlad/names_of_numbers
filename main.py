6a17123e15d595c269ca567a2ce6e118f3df35f9
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2025-01-11 01:18:46 -0800

    numbers

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def number_to_name(n):
     ones=["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     teens=["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
     tens=["","","twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "]
+    hundred=[""]
     
     return tens[n // 10] + (teens[n%10] if n%100//10 == 1 else ones[n%10]) + ("zero" if n == 0 else "")
 
